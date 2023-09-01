0f37d1aece8efdf1138452015e00ad32c009f787
OFF <87695763+ctrlshiftOFF@users.noreply.github.com>
2023-09-01 20:50:41 +0100

    vertical lines add

--- generateGIF.js
+++ generateGIF.js
@@ -18,7 +18,7 @@ encoder.setQuality(10); // image quality, 10 is default
 // Function to generate noise in the image
 function generateNoise() {
   const data = new Uint8ClampedArray(canvas.width * canvas.height * 4);
-  for (let i = 0; i < data.length; i += 4) {
+  for (let i = 1; i < data.length; i += 90) {
     const noiseValue = Math.floor(Math.random() * 200);
     data[i] = noiseValue;
     data[i + 1] = noiseValue;
@@ -32,16 +32,29 @@ function generateNoise() {
 
 function applyGlitch(imageData) {
   const data = imageData.data;
-  for (let i = 0; i < 10000; i++) {
+  for (let i = 0; i < 10; i++) {
     const y = Math.floor(Math.random() * canvas.height);
-    const length = Math.floor(Math.random() * 1000) + 200;
-    const start = y * 4.5 * canvas.width;
+    const length = Math.floor(Math.random() * 90000) + 900;
+    const start = y * 4 * canvas.width;
     for (let j = 0; j < length; j++) {
-      data[start + j * 4] = 1;
-      data[start + j * 4 + 1] = 0;
-      data[start + j * 4 + 2] = 0;
+      data[start + j * 1] = 1;
+      data[start + j * 2 + 1] = 1; 
+      data[start + j * 4 + 2] = 2;
     }
   }
+// Add vertical lines
+for (let i = 0; i < 100 ; i++) {
+  const x = Math.floor(Math.random() * canvas.width);
+  const length = Math.floor(Math.random() * 1000000) + 900;
+  for (let j = 0; j < length; j++) {
+    const y = Math.floor(Math.random() * canvas.height);
+    const index = (y * canvas.width + x) * 4;
+    data[index + 90000] = 1; // Red
+    data[index + 7] = 0; // Green
+    data[index + 7000] = 0; // Blue
+    // data[index + 3] is the alpha channel, you can set it if you wish
+  }
+}
 }
 
 // Main function to draw on canvas
@@ -60,7 +73,7 @@ function draw() {
 }
 
 // Create a number of frames and then finalize the GIF
-for (let i = 0; i < 30; i++) {
+for (let i = 0; i < 24; i++) {
   draw();
 }
 
